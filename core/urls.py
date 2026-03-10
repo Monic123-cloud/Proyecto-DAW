bfabf5d1309c26e0bd50fbde33602048c9803e31
Monica <monica.mblancom@gmail.com>
2026-03-10 11:37:03 +0100

    Configuración urls

--- core/urls.py
+++ core/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("", include("buscador.urls")),
+    path("api/buscador/", include("buscador.urls")),  # <--- Añade esto aquí
 ]
