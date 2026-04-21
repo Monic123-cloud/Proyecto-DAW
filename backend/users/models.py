5b442c7a7cb0454ecc8cb0eedefff597355f23c4
Monica <monica.mblancom@gmail.com>
2026-04-21 12:06:15 +0200

    removed non-existent cif_nif from superuser creator

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,6 +8,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError("Email es obligatorio")
 
         email = self.normalize_email(email)
+        cif_nif = models.CharField(max_length=20, unique=True)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -16,6 +17,9 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+
+        if not extra_fields.get("cif_nif"):
+            extra_fields["cif_nif"] = "admin1234"
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
