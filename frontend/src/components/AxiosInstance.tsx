72e611180003a21e2de9bb475b5bce8b8303e49d
Monica <monica.mblancom@gmail.com>
2026-05-03 18:31:59 +0200

    Fix: actualizando URL de la API en AxiosInstance

--- frontend/src/components/AxiosInstance.tsx
+++ frontend/src/components/AxiosInstance.tsx
@@ -1,11 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = 'http://127.0.0.1:8000/api'
+const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api";
 
 const AxiosInstance = axios.create({
-    baseURL: baseUrl,
-    timeout: 15000,
-    
-})
+  baseURL: baseUrl,
+  timeout: 15000,
+});
 
-export default AxiosInstance
+export default AxiosInstance;
