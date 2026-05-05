b5d4e473b08c0338dcaf23d2bc5e56374da666b8
neomemphys <151969537+neomemphys@users.noreply.github.com>
2026-05-05 11:13:28 +0200

    Subo about corregido

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,7 +1,6 @@
-import Header from "@/components/header";
 import About from "../../components/about";
 
 export default function aboutPage() {
   return (
-  <><Header /><About /></>);
+  <><About /></>);
 }
